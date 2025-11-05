3b9c59d9df81922e86c00252c4ccdac47129ae43
Lioneriod <123819366+Lioneriod@users.noreply.github.com>
2025-11-05 01:37:13 -0300

    add card images and files

---
+++ tarot-api/cards.js
@@ -0,0 +1,40 @@
+const majorArcana = [
+  {
+    id: 0,
+    name: "The Fool",
+    image: "./assets/the-fool.webp",
+    meanings: {
+      general: {
+        upright:
+          "beginnings, freedom, innocence, originality, adventure, idealism, spontaneity",
+        reversed:
+          "reckless, careless, distracted, naive, foolish, gullible, stale, dull",
+      },
+      love: {
+        upright: "new relationship, fun light romance",
+        reversed: "lack of commitment, risky relationship",
+      },
+      career: {
+        upright: "new job, new business, breath of fresh air at work",
+        reversed: "stale and boring job, reckless actions at work",
+      },
+      finances: {
+        upright: "spontaneous spending, financial opportunities",
+        reversed: "caution around financial opportunities, foolish purchases",
+      },
+      feelings: {
+        upright:
+          "excited, curious, adventurous, optimistic, free-spirited, open hearted, hopeful, spontaneous",
+        reversed:
+          "hesitant, uncertain, overwhelmed by fears, guarded, questioning feelings, apprehensive about connection",
+      },
+      actions: {
+        upright:
+          "embrace spontaneity, take risks, explore new experiences, trust your instincts, adopt a fresh perspective",
+        reversed:
+          "evaluate your goals, reflect your intentions, seek advice from mentors, take small steps, ground yourself",
+      },
+    },
+  },
+];
+module.exports = majorArcana;
